bc62da1fd077c402e6661b97e573299f8f208ad5
NAZIK-Dairbekovna <nazikdairbekova2@gmail.com>
2023-12-20 23:18:52 +0600

    login form

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -9,12 +9,31 @@ function Login() {
                      style={{width: "650px", height: "580px"}}
                      alt=""
                 />
-                <form action=""
-                      aria-placeholder={'Имя пользователя'}
-                      className="w-22 h-20 mt-60 ml-40 text-gray-500 border-b-blue-600"
-                >
-                    Имя пользователя
-                </form>
+                <div className='w-22 h-20 mt-60 ml-40'>
+                    <form action=""
+                          aria-placeholder={'Имя пользователя'}
+                          className=" text-gray-500 border-b-blue-600"
+                    >
+                        Имя пользователя
+                    </form>
+                    <form action=""
+                          aria-placeholder={'Пароль'}
+                          className=" text-gray-500 border-b-blue-600"
+                    >
+                        Пароль
+                    </form>
+                    <div className='flex flex-col'>
+                        <button className='flex self-start text-blue-800'>
+                            Забыли пароль
+                        </button>
+                        <button className='bg-gray-300 text-white font-bold rounded-2xl'>
+                            Войти
+                        </button>
+                        <button>
+                            Зарегистрироваться
+                        </button>
+                    </div>
+                </div>
             </div>
         </>
     );
