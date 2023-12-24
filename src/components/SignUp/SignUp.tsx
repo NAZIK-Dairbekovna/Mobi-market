83b549315acf79f1ad2f760e6b45c95afd7dd488
Nazik Dairbekova <113763454+NAZIK-Dairbekovna@users.noreply.github.com>
2023-12-24 22:36:38 +0600

    style for formic (#7)

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -6,10 +6,10 @@ const SignUp = () => {
         <>
             <div className='flex justify-items-stretch'>
                 <img src={login}
-                     style={{width: "650px", height: "580px"}}
+                     style={{width: "550px", height: "590px"}}
                      alt=""
                 />
-                <div className='w-22 h-20 mt-60 ml-40'>
+                <div className='w-22 h-20 mt-60 ml-60'>
                     <form action="text-table"
                           aria-placeholder={'Имя пользователя'}
                           className=" text-gray-500 border-b-blue-600"
